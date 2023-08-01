bb57efe73e40d03ec56b10be14d9978256641bd7
ErnestsEida <ernestseida14@gmail.com>
2023-08-01 20:39:14 +0300

    Renderer and Animations done

--- core/includes/Animation.cpp
+++ core/includes/Animation.cpp
@@ -1,3 +1,85 @@
 #pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Alerts.cpp"
 
-class Animation {};
+using namespace std;
+using namespace sf;
+
+class Animation {
+private:
+    Clock timer;
+    float animation_speed;
+    int frame_count;
+    Vector2i frame_size;
+    Sprite* sprite = nullptr;
+    bool loop = true;
+    Texture texture;
+
+    bool freezeFrame = false;
+public:
+    int current_frame = 0;
+
+    Animation(string path_to_spritesheet, Vector2i frame_size, int frame_count, bool loop) {
+        this->frame_count = frame_count;
+        this->frame_size = frame_size;
+        this->animation_speed = frame_count;
+        this->loop = loop;
+
+        this->texture = Texture();
+        if (!this->texture.loadFromFile(path_to_spritesheet)) Alerts::Error("Couldn't find a spritesheet at: " + path_to_spritesheet);
+
+        this->sprite = new Sprite(this->texture, IntRect(Vector2i(0,0), frame_size));
+    }
+
+    void Freeze() {
+        this->freezeFrame = true;
+    }
+
+    void Unfreeze() {
+        this->freezeFrame = false;
+    }
+
+    void ToggleFreeze() {
+        this->freezeFrame = !this->freezeFrame;
+    }
+
+    void SetLoop(bool loop) {
+        this->loop = loop;
+    }
+
+    void SetAnimationSpeed(float animation_speed) {
+        this->animation_speed = animation_speed;
+    }
+
+    Sprite* GetFrame() {
+        return this->sprite;
+    }
+
+    Vector2i GetSize() {
+        return this->frame_size;
+    }
+
+    void NextFrame() {
+        IntRect textureRect = this->sprite->getTextureRect();
+
+        if (this->timer.getElapsedTime().asSeconds() > 1 / this->animation_speed) {
+            if (textureRect.left + this->frame_size.x >= (this->frame_count * this->frame_size.x)) {
+                if (loop) {
+                    textureRect.left = 0;
+                    this->current_frame = 0;
+                } else {
+                    this->Freeze();
+                }
+            } else {
+                if (!this->freezeFrame) {
+                    textureRect.left += this->frame_size.x;
+                    this->current_frame++;
+                }
+            }
+            this->timer.restart();
+        }
+
+        this->sprite->setTextureRect(textureRect);
+    }
+};
--- core/includes/Renderer.cpp
+++ core/includes/Renderer.cpp
@@ -1,7 +1,53 @@
 #pragma once
 #include "Gameobject.cpp"
+#include "Animation.cpp"
+
+enum ShapeType {
+  Circle,
+  Rectangle,
+};
 
 class Renderer : public GameObject {
+private:
+  Drawable* mainDrawable = nullptr;
+  Animation* animation = nullptr;
+
 public:
-  void Update() override {}
+  void SetShape(Vector2f size = Vector2f(10, 10), ShapeType shape_type = ShapeType::Circle) {
+    if (shape_type == ShapeType::Circle) {
+      CircleShape* shape = new CircleShape(size.x);
+      this->mainDrawable = shape;
+    } else {
+      RectangleShape* shape = new RectangleShape(size);
+      this->mainDrawable = shape;
+    }
+  }
+
+  void SetShape(Shape* shape) {
+    this->mainDrawable = shape;
+  }
+
+  void SetAnimation(Animation* animation) {
+    this->animation = animation;
+    this->mainDrawable = this->animation->GetFrame();
+  }
+
+  void FreeDrawables() {
+    this->mainDrawable = nullptr;
+    this->drawables.clear();
+    this->drawables.shrink_to_fit();
+  }
+
+  void Update() override {
+    if (this->mainDrawable != nullptr && this->drawables.size() == 0) {
+      this->PushDrawable(this->mainDrawable);
+    } else if (this->mainDrawable == nullptr && this->drawables.size() > 0) {
+      this->FreeDrawables();
+    } else {
+      if (this->animation != nullptr) {
+        this->mainDrawable = this->animation->GetFrame();
+        this->animation->NextFrame();
+      }
+    }
+  }
 };
