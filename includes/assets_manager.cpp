c4b5e420f229d15579b4376db8ebb7fc2333ab7f
ErnestsEida <ernestseida14@gmail.com>
2023-08-19 11:54:37 +0300

    Basic asset manager tools

--- includes/assets_manager.cpp
+++ includes/assets_manager.cpp
@@ -2,6 +2,7 @@
 #include "interfaces/IDisplayWindow.cpp"
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -44,12 +45,11 @@ struct DrawnObject {
 class AssetsManager : public IDisplayWindow {
 private:
   bool draw_area_set = false;
-  bool draw_tools_set = false;
   bool is_drawing = false;
   
   Tool selected_tool;
   vector<DrawnObject> drawn_objects = vector<DrawnObject>();
-  DrawnObject current_object;
+  ImVec2 last_point;
 
   ////////////////////////////// FLAGS /////////////////////////////
 
@@ -66,45 +66,41 @@ private:
   }
 
   void HandleMouseDown(ImVec2 mouse_pos) {
-    if (selected_tool.type == ToolType::Pencil) {
-      this->drawn_objects.push_back(DrawnObject(this->selected_tool, mouse_pos));
-    } else if (selected_tool.type == ToolType::Line) {
-
-    } else if (selected_tool.type == ToolType::Rectangle) {
-
-    } else if (selected_tool.type == ToolType::Circle) {
-
-    }
+    this->last_point = mouse_pos;
   }
 
   void HandleMouseDragging(ImVec2 mouse_pos) {
     if (selected_tool.type == ToolType::Pencil) {
-      this->drawn_objects.push_back(DrawnObject(this->selected_tool, mouse_pos));
+      this->drawn_objects.push_back(DrawnObject(this->selected_tool, this->last_point, mouse_pos));
+      this->last_point = mouse_pos;
     } else if (selected_tool.type == ToolType::Line) {
-
+      // Preview
     } else if (selected_tool.type == ToolType::Rectangle) {
-
+      // Preview
     } else if (selected_tool.type == ToolType::Circle) {
-
+      // Preview
     }
   }
 
   void HandleMouseRelease(ImVec2 mouse_pos) {
-    if (selected_tool.type == ToolType::Line) {
-
-    } else if (selected_tool.type == ToolType::Rectangle) {
-
-    } else if (selected_tool.type == ToolType::Circle) {
-
+    if (!selected_tool.type == ToolType::Pencil) {
+      this->drawn_objects.push_back(DrawnObject(this->selected_tool, this->last_point, mouse_pos));
     }
   }
 
   void DrawToCanvas(ImDrawList *draw_list, ImVec2 canvas_pos) {
     for(size_t i = 0; i < this->drawn_objects.size(); i++) {
       DrawnObject object = this->drawn_objects.at(i);
-      if (object.drawn_by == ToolType::Pencil) {
-        ImVec2 adjusted_vec = ImVec2(canvas_pos.x + object.start.x, canvas_pos.y + object.start.y);
-        draw_list->AddCircleFilled(adjusted_vec, object.size, object.color);
+      ImVec2 adjusted_end_vec = ImVec2(canvas_pos.x + object.end.x, canvas_pos.y + object.end.y);
+      ImVec2 adjusted_start_vec = ImVec2(canvas_pos.x + object.start.x, canvas_pos.y + object.start.y);
+      
+      if (object.drawn_by == ToolType::Pencil || object.drawn_by == ToolType::Line) {
+        draw_list->AddLine(adjusted_start_vec, adjusted_end_vec, object.color, object.size);
+      } else if (object.drawn_by == ToolType::Rectangle) {
+        draw_list->AddRect(adjusted_start_vec, adjusted_end_vec, object.color, 0, 0, object.size);
+      } else if (object.drawn_by == ToolType::Circle) {
+        int radius = adjusted_end_vec.x - adjusted_start_vec.x;
+        draw_list->AddCircle(adjusted_start_vec, radius > 0 ? radius : -radius, object.color, 0, object.size);
       }
     }
   }
@@ -145,23 +141,40 @@ private:
     ImGui::End();
   }
 
-  void ShowDrawingControls() {
-    if (!draw_tools_set) {
-      ImGui::SetNextWindowSize(ImVec2(300, 400));
-      draw_tools_set = true;
+  void ShowControlButtons() {
+    if (ImGui::Button("Save Asset")) {
+      // Save asset
     }
 
-    ImGui::Begin("Canvas controls", &this->is_open);
-      if (ImGui::Button("Save Asset", ImVec2(225, 40))) {
-        // Save asset
-      }
+    ImGui::SameLine();
+    if (ImGui::Button("New Asset")) {
+      // New Asset
+    }
+  }
 
-      if (ImGui::Button("New Asset", ImVec2(225, 40))) {
-        // New Asset
-      }
+  void ShowDrawingControls() {
+    ImGui::SetNextWindowSize(ImVec2(250, 400));
+
+    ImGui::Begin("Canvas controls", &this->is_open);
+      ShowControlButtons();
       
       ImGui::ColorPicker4("##color_picker", this->selected_tool.color, ColorPickerFlags());
       ImGui::SliderInt("Tool Size", &this->selected_tool.size, 1, 25);
+      if (ImGui::BeginListBox("##Tool")) {
+        if (ImGui::Selectable("Pencil", this->selected_tool.type == ToolType::Pencil)) {
+          this->selected_tool.type = ToolType::Pencil;
+        }
+        if (ImGui::Selectable("Rectangle", this->selected_tool.type == ToolType::Rectangle)) {
+          this->selected_tool.type = ToolType::Rectangle;
+        }
+        if (ImGui::Selectable("Line", this->selected_tool.type == ToolType::Line)) {
+          this->selected_tool.type = ToolType::Line;
+        }
+        if (ImGui::Selectable("Circle", this->selected_tool.type == ToolType::Circle)) {
+          this->selected_tool.type = ToolType::Circle;
+        }
+        ImGui::EndListBox();
+      }
     ImGui::End();
   }
 
